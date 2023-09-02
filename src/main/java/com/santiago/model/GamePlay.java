1c07c6fad82cac88402c203df7656d81b8f80079
Aldo Santiago <aldo11santiago@gmail.com>
2023-09-01 20:00:50 -0600

    add new GamePlay model

---
+++ src/main/java/com/santiago/model/GamePlay.java
@@ -0,0 +1,11 @@
+package com.santiago.model;
+
+import lombok.Data;
+
+@Data
+public class GamePlay {
+    private TicToe type;
+    private Integer coordinateX;
+    private Integer coordinateY;
+    private String gameId;
+}
